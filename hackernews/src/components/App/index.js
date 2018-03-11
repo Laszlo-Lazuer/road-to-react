b2e8076fe57f00006dc9d2c2afa2518e3922b707
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-11 15:19:07 -0500

    Lint Cleanup & modularizes constants.

--- hackernews/src/components/App/index.js
+++ hackernews/src/components/App/index.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import Button from '../Button';
 import Search from '../Search';
 import Table from '../Table';
-
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from '../../constants';
 require('./index.css');
 
-const DEFAULT_QUERY = 'squirrel';
-const DEFAULT_HPP = '100';
 
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   constructor(props) {
