04404e1c85f69c1b5abafcd0fad5af7e50918b2a
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-11 01:28:09 -0600

    Split up components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,82 +1,111 @@
 import React, { Component } from 'react';
-import './App.css';
+require('./App.css');
 
 const list = [
-{
-  title: 'React',
-  url: 'https://facebook.github.io/react/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-},
-{
-  title: 'Redux',
-  url: 'https://github.com/reactjs/redux',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-},
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
 ];
 
-const animal = 'squirrel';
-
 const isSearched = (searchTerm) => (item) =>
-!searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       list,
-      animal,
       searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
-  onSearchCHange = (event) => {
-      this.setState({ searchTerm: event.target.value});
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
   }
 
-  onDismiss = (id) => {
-    const isNotID = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotID);
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
   }
 
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="App">
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
 
-render() {
-  const { searchTerm, list} = this.state;
-  return (
-    <div className="App">
-      <span>{this.state.animal}</span>
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
       <form>
-        <input type='text' onChange={this.onSearchCHange}
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
         />
       </form>
-      {list.filter(isSearched(searchTerm)).map(item => {
-        return (
-        <div key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button
-              onClick={() => this.onDismiss(item.objectID)}
-              type='button'
-            >
-            Dismiss
-            </button>
-          </span>
-        </div>
-      );
-      })}
-    </div>
-  );
+    );
+  }
 }
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
